b18a42e09b89b5e747b47d5e707920526b1284f8
SomathSatou <tomsaout@wanadoo.fr>
2018-11-28 18:15:29 +0100

    avancé sur l'implémentation de la file d'annotation

--- app/src/main/java/com/master/info_ua/videoannottool/annotation/ControlerAnnotation.java
+++ app/src/main/java/com/master/info_ua/videoannottool/annotation/ControlerAnnotation.java
@@ -1,47 +1,75 @@
 package com.master.info_ua.videoannottool.annotation;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.master.info_ua.videoannottool.MainActivity;
 import com.master.info_ua.videoannottool.annotation.Annotation;
 import com.master.info_ua.videoannottool.annotation.VideoAnnotation;
 import com.master.info_ua.videoannottool.Ecouteur;
 import com.master.info_ua.videoannottool.annotation.infoAnno;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
-public class ControlerAnnotation {
-
+public class ControlerAnnotation implements Runnable {
+    private Context _mainActivity;
     private Ecouteur m_ecouteur = null;
-    private ArrayList<infoAnno> ListInfoAnno;
-    private VideoAnnotation listAnno;
+    private ArrayList<infoAnno> ListInfoAnno = new ArrayList<infoAnno>();
+    private List<Annotation> listAnno;
     private int last_pos = 0;
     private List<Integer> annotActive;
+    private boolean cancelled = false;
 
-    public ControlerAnnotation(Ecouteur m_e, VideoAnnotation videoAnnotation){
+    public void setLast_pos(int last_pos) {
+        this.last_pos = last_pos;
+    }
+
+    public ControlerAnnotation(Context context,Ecouteur m_e, VideoAnnotation videoAnnotation){
+        _mainActivity = context;
         m_ecouteur = m_e;
-        listAnno = videoAnnotation;
+        listAnno = videoAnnotation.getAnnotationList();
         //ttt de videoAnnotation
+        int i = 0;
+        for( Annotation elt : listAnno){
+            infoAnno tmp =new infoAnno(arronditSeconde(elt.getAnnotationStartTime()),i,true);
+            ListInfoAnno.add(tmp);
+            long fin = arronditSeconde(elt.getAnnotationStartTime()+elt.getAnnotationDuration());
+            tmp = new infoAnno(fin,i,false);
+            ListInfoAnno.add(tmp);
+        }
+        Collections.sort(ListInfoAnno);
+
     }
 
     public void updateAnno(VideoAnnotation update){
-        listAnno = update;
+        listAnno = update.getAnnotationList();
     }
 
     // verifie si il existe des annotation qui n'ont pas été lancer depuis le dernier appel et positionne le curseur sur le prochain element a vérifier
     // stop aussi les annotation qui doive se terminer
-    public void checkTime(){
+    public boolean checkTime(){
         //verifier le temps et lancer launch avec le paramétre correspondant
-        while((last_pos<ListInfoAnno.size()&&(arronditSeconde(m_ecouteur.getVideoTime())<ListInfoAnno.get(last_pos).getTime()))){
-            if(arronditSeconde(m_ecouteur.getVideoTime())==ListInfoAnno.get(last_pos).getTime()) {
-                if (ListInfoAnno.get(last_pos).isDebut()) {
-                    launch(listAnno.getAnnotationList().get(ListInfoAnno.get(last_pos).getIndex()));
-                    annotActive.add(ListInfoAnno.get(last_pos).getIndex());
-                } else {
-                    stop(listAnno.getAnnotationList().get(ListInfoAnno.get(last_pos).getIndex()));
-                    annotActive.remove(ListInfoAnno.get(last_pos).getIndex());
+        if(m_ecouteur.getPlayer().getDuration()!=m_ecouteur.getVideoTime()){
+            while((last_pos<ListInfoAnno.size()&&(arronditSeconde(m_ecouteur.getVideoTime())<ListInfoAnno.get(last_pos).getTime()))){
+                if(arronditSeconde(m_ecouteur.getVideoTime())==ListInfoAnno.get(last_pos).getTime()) {
+                    if (ListInfoAnno.get(last_pos).isDebut()) {
+                        launch(listAnno.get(ListInfoAnno.get(last_pos).getIndex()));
+                        annotActive.add(ListInfoAnno.get(last_pos).getIndex());
+                    } else {
+                        stop(listAnno.get(ListInfoAnno.get(last_pos).getIndex()));
+                        annotActive.remove(ListInfoAnno.get(last_pos).getIndex());
+                    }
                 }
+                last_pos++;
             }
-            last_pos++;
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
@@ -96,38 +124,66 @@ public class ControlerAnnotation {
     }
 
     public void launchDraw(){
+        Toast.makeText((Context)m_ecouteur,"je lance l'annotaion dessin",Toast.LENGTH_LONG);
         //encore du travail
     }
 
     public void launchText(){
+        Toast.makeText((Context)m_ecouteur,"je lance l'annotaion text",Toast.LENGTH_LONG);
         //encore du travail
     }
 
     public void launchZoom(){
+        Toast.makeText((Context)m_ecouteur,"je lance l'annotaion zoom",Toast.LENGTH_LONG);
         //encore du travail
     }
 
     public void launchAudio(){
+        Toast.makeText((Context)m_ecouteur,"je lance l'annotaion audio",Toast.LENGTH_LONG);
         //encore du travail
     }
 
     public void launchSlowmo(float slow){
+        Toast.makeText((Context)m_ecouteur,"je lance l'annotaion ralenti",Toast.LENGTH_LONG);
         m_ecouteur.setSpeed(slow);
     }
 
     public void stopDraw(){
+        Toast.makeText((Context)m_ecouteur,"je stop l'annotaion dessin",Toast.LENGTH_LONG);
         //encore du travail
     }
     public void stopText(){
+        Toast.makeText((Context)m_ecouteur,"je stop l'annotaion text",Toast.LENGTH_LONG);
         //encore du travail
     }
     public void stopZoom(){
+        Toast.makeText((Context)m_ecouteur,"je stop l'annotaion zoom",Toast.LENGTH_LONG);
         //encore du travail
     }
     public void stopAudio(){
+        Toast.makeText((Context)m_ecouteur,"je stop l'annotaion audio",Toast.LENGTH_LONG);
         //encore du travail
     }
     public void stopSlowmo(){
+        Toast.makeText((Context)m_ecouteur,"je stop l'annotaion ralenti",Toast.LENGTH_LONG);
         m_ecouteur.setSpeed(1f);
+    }
+
+    @Override
+    public void run() {
+        while(checkTime()&&(!cancelled)){
+            synchronized (this) {
+                try {
+                    System.out.print("je suis bien lancer");
+                    this.wait(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void cancel(){
+        cancelled = true;
     }
 }
--- app/src/main/java/com/master/info_ua/videoannottool/annotation/infoAnno.java
+++ app/src/main/java/com/master/info_ua/videoannottool/annotation/infoAnno.java
@@ -1,6 +1,26 @@
 package com.master.info_ua.videoannottool.annotation;
 
-public class infoAnno{
+public class infoAnno implements Comparable{
+    private long time;
+    private int index;
+    private boolean debut; // true pour launch() et false pour stop()
+
+    public infoAnno(long _time,int _index,boolean _debut){
+        time = _time;
+        index = _index;
+        debut = _debut;
+    }
+
+    @Override
+    public int compareTo(Object comparestu) {
+        int compareTime=(int)((infoAnno)comparestu).getTime();
+        /* For Ascending order*/
+        return (int)(this.time)-compareTime;
+
+        /* For Descending order do like this */
+        //return compareage-this.studentage;
+    }
+
     public long getTime() {
         return time;
     }
@@ -13,7 +33,5 @@ public class infoAnno{
         return debut;
     }
 
-    private long time;
-    private int index;
-    private boolean debut; // true pour launch() et false pour stop()
+
 }
