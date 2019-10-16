e79f28a505756b47e7a2ad7fdfdc225ac9d46d0e
anas <anas.tagueniti@gmail.com>
2019-10-16 14:47:44 +0200

    rien

--- app/src/main/java/com/master/info_ua/videoannottool/MainActivity.java
+++ app/src/main/java/com/master/info_ua/videoannottool/MainActivity.java
@@ -33,6 +33,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.PlaybackParameters;
